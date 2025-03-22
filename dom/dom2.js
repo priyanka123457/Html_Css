203f646aa911b6ef5a28ad8309baed97a965d66f
priyanka123457 <senp4891@gmail>
2025-03-22 16:44:20 +0530

    html css files

---
+++ dom/dom2.js
@@ -0,0 +1,48 @@
+// let div = document.querySelector("div");
+// console.log(div);
+// let id = div.getAttribute("id");
+// console.log(id)
+// let para = document.querySelector("p");
+// console.log(para.setAttribute("class","newclass"))
+// let div = document.querySelector("div")
+// change the bacground color in di tag using js
+// div.style.backgroundColor = "blue"
+
+// can change fonsize also
+// div.style.fontSize = "26px"
+// can change text also
+// div.innerText = "hello"
+// how can be insert element new element before adding first will create dom manupilation
+// let newbtn = document.createElement("button");
+// newbtn.innerText = "click me"
+// console.log(newbtn);
+// let p = document.querySelector("p");
+// div1.append(newbtn);
+// add the element
+// div1.prepend(newbtn);
+// div1.before(newbtn)
+// div ke just bad add krta hai
+// p.after(newbtn)
+// newbtn = document.createElement("button");
+// newbtn.innerText = "click me";
+// newbtn.style.color = "red";
+// newbtn.style.backgroundcolor = "blue";
+// document.querySelector("body").prepend(newbtn);
+// console.log(newbtn)
+newbtn = document.createElement("p") 
+console.log(newbtn)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
